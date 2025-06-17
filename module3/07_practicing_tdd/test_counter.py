3a6586e88b24734762145db43cb31dad4657dda5
amos-maganyane <amos.maganyane@enviro365.co.za>
2025-06-17 12:00:31 +0200

    Add initial practicing tdd lab skeleton & implement update a counter method with tests

---
+++ module3/07_practicing_tdd/test_counter.py
@@ -0,0 +1,40 @@
+"""
+Test Cases for Counter Web Service
+"""
+from unittest import TestCase
+import status
+from counter import app
+
+class CounterTest(TestCase):
+    """Test Cases for Counter Web Service"""
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        result = self.client.post("/counters/foo")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        data = result.get_json()
+        self.assertIn("foo", data)
+        self.assertEqual(data["foo"], 0)
+
+    def test_duplicate_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post("/counters/bar")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post("/counters/bar")
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+
+    def test_update_a_counter(self):
+        """It should increment the counter"""
+        result = self.client.post("/counters/baz")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        data = result.get_json()
+        baseline = data["baz"]
+        # Update the counter
+        result = self.client.put("/counters/baz")
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        data = result.get_json()
+        self.assertEqual(data["baz"], baseline + 1)    
